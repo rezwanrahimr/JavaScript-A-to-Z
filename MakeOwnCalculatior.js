feaff2504275be7c5f487ee560143e5c92b53f2d
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-02 22:34:18 +0600

    pratice arethmetic oparator

---
+++ MakeOwnCalculatior.js
@@ -0,0 +1,28 @@
+
+//Task-3
+var firstNumber = prompt("Enter First Number : ");
+var secondNumber = prompt("Enter Second Number : ");
+
+firstNumber = Number(firstNumber);
+secondNumber = Number(secondNumber);
+
+// addition
+var addition = firstNumber + secondNumber;
+document.write(firstNumber + " + " + secondNumber + " = " + addition + "<br/>");
+
+// minus
+var minus = firstNumber - secondNumber;
+document.write(firstNumber + " - " + secondNumber + " = " + minus + "<br/>"); 
+
+// multiply
+var multiply = firstNumber - secondNumber;
+document.write(firstNumber + " * " + secondNumber + " = " + multiply + "<br/>");
+
+// division
+var division = firstNumber / secondNumber;
+document.write(firstNumber + " / " + secondNumber + " = " + division + "<br/>");
+
+// modulus
+var modulus = firstNumber % secondNumber;
+document.write(firstNumber + " % " + secondNumber + " = " + modulus + "<br/>");
+
