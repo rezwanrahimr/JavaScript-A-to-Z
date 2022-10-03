5e7df6a75cdb6039941a5fd0e34e180c1a908703
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-03 14:23:35 +0600

    for loop

---
+++ ForLoop.js
@@ -0,0 +1,18 @@
+
+for(var i=0; i<10; i++){
+    console.log(i);
+}
+
+// 1 to 100 odd number.
+for(var i = 1; i<=99; i=i+2){
+    console.log(i);
+}
+//1 to 100 even number.
+for(var i = 2; i<=100; i=i+2){
+    console.log(i);
+}
+
+// 99 to 1 number .
+for(var i= 99; i>=1; i--){
+    console.log(i);
+}
