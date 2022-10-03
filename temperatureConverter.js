412622eb50ab5378bae6c259e1b27d6096667ad8
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-03 11:00:24 +0600

    celsius to fahrenheit conveter

--- temperatureConverter.js
+++ temperatureConverter.js
@@ -3,3 +3,8 @@
 var Fahrenheit = prompt("Enter Fahrenheit : ");
 var Celsius = Fahrenheit - 32 * (5/9);
 console.log("Celsius" + Celsius);
+
+// Celsius to Fahrenheit.
+var celsius = prompt("Enter Celsius : ");
+var fahrenheit = celsius * (9/5) + 32;
+document.write("Fahrenheit: " + fahrenheit);
