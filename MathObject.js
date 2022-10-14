6a6535fdc213f095d8f940c34bbaf95a756ed7ab
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-14 09:43:04 +0600

    math object

---
+++ MathObject.js
@@ -0,0 +1,12 @@
+// math object
+
+console.log(Math.floor(2.9));
+console.log(Math.ceil(2.9));
+console.log(Math.round(2.9));
+console.log(Math.max(45,80,99));
+
+// random number
+console.log(Math.floor(Math.random() * 6));
+console.log(Math.floor(Math.random() * 5) + 1);
+
+console.log(Math.floor(Math.random() * 6) + 10)
