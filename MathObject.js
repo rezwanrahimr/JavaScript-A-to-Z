768ab97cab5200b92a599b86c8da598fcc5df36e
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-22 17:03:59 +0600

    date object

--- MathObject.js
+++ MathObject.js
@@ -1,12 +1,11 @@
 // math object
-
 console.log(Math.floor(2.9));
 console.log(Math.ceil(2.9));
 console.log(Math.round(2.9));
-console.log(Math.max(45,80,99));
+console.log(Math.max(45, 80, 99));
 
 // random number
 console.log(Math.floor(Math.random() * 6));
 console.log(Math.floor(Math.random() * 5) + 1);
 
-console.log(Math.floor(Math.random() * 6) + 10)
+console.log(Math.floor(Math.random() * 6) + 10);
---
+++ date_object.js
@@ -0,0 +1,15 @@
+var date = new Date();
+console.log(date);
+
+// get month.
+var month = date.getMonth();
+console.log(month);
+// get year.
+var year = date.getFullYear();
+console.log(year + " year");
+// get day
+var day = date.getDay();
+console.log(day);
+// get current time
+var time = date.getHours();
+console.log(time);
