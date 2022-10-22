1f5c768d0bc6cb3ea94f57a023a669e5fcada63e
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-22 22:23:05 +0600

    on click

--- DOM.js
+++ DOM.js
@@ -1,10 +1,11 @@
 var myName = document.getElementById("name");
 myName.innerHTML = "hello rezwan";
-
+// get element by id.
 var welcomeMessage = (document.getElementById("welcome").innerHTML =
   "welcome to js");
-
+// get element by class.
 var byeMessage = (document.getElementsByClassName("bye")[0].innerHTML = "bye");
+// get element by tag name.
 var tag = (document.getElementsByTagName("h1")[1].innerHTML = "tag");
 
 // use quary selector by id.
@@ -18,4 +19,10 @@ for (var i = 0; i <= 3; i++) {
   //   use quary selector all .
   var useQuaryByTag = (document.querySelectorAll("h1")[i].innerHTML =
     "Hello WOrld");
+}
+
+function clickMe() {
+  var click = document.querySelector("#clickMe");
+  click.src =
+    "https://th.bing.com/th/id/OIP.NMHAU2Gll4qBVSlAOOsIUwEsDI?pid=ImgDet&rs=1";
 }
