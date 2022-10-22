2eed344e5b5617de0e142e8dc8d8b0d6be797918
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-22 21:52:25 +0600

    use quary selector

--- DOM.js
+++ DOM.js
@@ -3,3 +3,19 @@ myName.innerHTML = "hello rezwan";
 
 var welcomeMessage = (document.getElementById("welcome").innerHTML =
   "welcome to js");
+
+var byeMessage = (document.getElementsByClassName("bye")[0].innerHTML = "bye");
+var tag = (document.getElementsByTagName("h1")[1].innerHTML = "tag");
+
+// use quary selector by id.
+var useQuary = (document.querySelector("#name").innerHTML =
+  "Hello,Rezwan Rahim ! How are you");
+// use quary selector by class.
+var useQuaryByClass = (document.querySelector(".bye").innerHTML =
+  "Welcome to my world");
+
+for (var i = 0; i <= 3; i++) {
+  //   use quary selector all .
+  var useQuaryByTag = (document.querySelectorAll("h1")[i].innerHTML =
+    "Hello WOrld");
+}
