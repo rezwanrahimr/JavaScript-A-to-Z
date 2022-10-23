5f04b5dbf824bbfdfab78921bffb7b9f4a20451a
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-23 12:33:45 +0600

    remove style

--- DaynamicStyle.js
+++ DaynamicStyle.js
@@ -2,3 +2,8 @@ function addStyle() {
   var name = document.getElementById("name");
   name.classList.add("name-text");
 }
+
+function removeStyle() {
+  var name = document.getElementById("name");
+  name.classList.remove("name-text");
+}
