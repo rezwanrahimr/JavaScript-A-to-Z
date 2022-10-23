a98c417cc5909f460ca242de73a9a5c16f1faa2a
rezwanrahimr <rezwanrahim69@gmail.com>
2022-10-23 10:30:38 +0600

    append child

---
+++ CREATE-ELEMENT.js
@@ -0,0 +1,20 @@
+// create html element by javaScript.
+var element = document.createElement("h1");
+var text = document.createTextNode("Hello there how are you");
+element.appendChild(text);
+
+var app = document.getElementById("add").appendChild(element);
+
+// remove html element by javascript.
+/* var cc = document.getElementsByTagName("h1")[0];
+
+document.getElementsByClassName("my-div").removeChild(); */
+
+// create element in before.
+/* var element1 = document.createElement("h1");
+var taxt1 = document.createTextNode("Allah always watching me");
+element1.appendChild(taxt1);
+
+var docc = document.getElementsByTagName("h1")[0];
+app.insertBefore(element1, app);
+ */
