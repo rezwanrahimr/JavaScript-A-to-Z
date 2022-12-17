a8be14bce50095c58603b9e2338d14858e660573
rezwanrahimr <rezwanrahim69@gmail.com>
2022-12-17 18:15:40 +0600

    nestad array

--- basic.js
+++ basic.js
@@ -1,2 +1,24 @@
-var a; // variable declearing.
-var a = 1; // initialize the variable.
+/* var a; // variable declearing.
+var a = 1; */ // initialize the variable.
+
+//addition assignment operator.
+/* var b = 1;
+b += 5; */
+// console.log(b);
+
+/* var name = 'rezwn"rahim"rupak';
+console.log(name); */
+
+/* var name = "rezwan rahim";
+name += " rupak";
+name.length - 1;
+console.log(name); */
+
+// array
+/* var arrayElement = [332, 54, 65, 4];
+console.log(arrayElement[1]); */
+
+// Nested array
+var arrayElement = [["rezwan", 31], ["rahim,01"]];
+var output = arrayElement[0][1];
+console.log(output);
