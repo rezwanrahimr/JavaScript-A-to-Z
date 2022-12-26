129d7be839fb111edfd7d0f42e37c97206ea312a
rezwanrahimr <rezwanrahim69@gmail.com>
2022-12-26 12:17:54 +0600

    FIND THE LONGEST ELEMENT IN ANY ARRAY AND RETURN THE INDEX NUMBER THIS ELEMENT

--- js problem/problem2.js
+++ js problem/problem2.js
@@ -1,21 +1,21 @@
 /*................................
 FIND THE SPACIFIC WORD IN A PARAGRAPS IN TEL THE FAST POSITION OF THIS WORD.
 */
-const sentense =
+/* const sentense =
   "Hello Everyone.This is rezwan rahim.I am a jr web developer.now I study at Dhaka International University in Computer Science & Engineering.rezwan before that I Complete my Diploma deagre in Feni Polytechnic Institute.";
 
 let matchs = sentense.match(/rezwan/gi);
 console.log(matchs ? matchs.length : 0);
 
 let search = sentense.search(/toma/i);
-console.log(search >= 0 ? search : "Not Found");
+console.log(search >= 0 ? search : "Not Found"); */
 
 /*.................................
 // input:linearsearch(['a','b','c','d','c'],'c')
 // output: 2 or 'not found'
 // problem: linearSearch() function টি  implement করে দেখান ।
 ..*/
-
+/* 
 function linearSearch(array, value) {
   let length = array.length;
   for (var i = 0; i < length; i++) {
@@ -26,4 +26,20 @@ function linearSearch(array, value) {
   return "Not Found";
 }
 
-console.log(linearSearch(["a", "b", "c", "d", "c"], "c"));
+console.log(linearSearch(["a", "b", "c", "d", "c"], "c")); */
+
+/*...................................
+FIND THE LONGEST ELEMENT IN ANY ARRAY AND RETURN THE INDEX NUMBER THIS ELEMENT
+..*/
+function longest(array) {
+  let current = "";
+  let length = array.length;
+  for (var i = 0; i < length; i++) {
+    if (array[i].length > current.length) {
+      current = array[i];
+    }
+  }
+  return [current, array.indexOf(current)];
+}
+
+console.log(longest(["hello", "how", "areyou"]));
