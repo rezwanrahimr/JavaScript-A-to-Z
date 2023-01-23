7ab1fa760917ad7f348e536f0a777a2dacdf3af0
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-23 18:28:51 +0600

    js popup

--- dom/script.js
+++ dom/script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+/* function myFunction() {
   let animation = document.getElementById("animate");
   let interval = setInterval(animate, 5);
   let count = 0;
@@ -12,3 +12,42 @@ function myFunction() {
     }
   }
 }
+ */
+
+/* function myFun(id) {
+  id.innerHTML = "This is Rezwan Rahim";
+}
+function how() {
+  document.getElementById("how-c").innerHTML = Date();
+}
+document.getElementById("how").onclick = how; */
+
+/* let date = document.getElementById("datee");
+date.addEventListener("click", myFun);
+function myFun() {
+  date.innerHTML = Date();
+}
+ */
+
+/* let date = document.getElementById("datee");
+date.addEventListener("click", myFun);
+date.addEventListener("mouseover", myFun2);
+function myFun() {
+  date.innerHTML = "Hello How are you";
+}
+function myFun2() {
+  date.innerHTML = "How can i help you";
+}
+window.addEventListener("resize", function () {
+  date.style.visibility = "hidden";
+});
+
+const result = document.getElementById("res");
+result.addEventListener("click", function () {});
+function calculation() {
+  result.innerHTML = 10 + 10;
+}
+function myFUn() {
+  document.getElementById("res").removeEventListener("click", calculation);
+}
+let node = document.getElementById("node").childNodes[1].nodeValue; */
