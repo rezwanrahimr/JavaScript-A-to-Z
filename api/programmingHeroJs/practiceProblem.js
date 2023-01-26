8fc78754212234404b2c1cae514d7ce6e3e5cb43
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-26 19:24:48 +0600

    pratice problme 2

--- api/programmingHeroJs/practiceProblem.js
+++ api/programmingHeroJs/practiceProblem.js
@@ -1,6 +1,15 @@
 // Pratice problme 1
-var takeMony = 1000,
+/* var takeMony = 1000,
   apples = 50,
   orange = 90;
 var result = takeMony - (apples + orange);
-console.log("Result : ", result);
+console.log("Result : ", result); */
+
+// pratice problme 2
+var mathematics = 80,
+  biology = 60,
+  chemistry = 100,
+  physics = 77,
+  bangla = 80;
+var calculate = (mathematics + biology + chemistry + physics + bangla) / 5;
+console.log(calculate.toFixed(2));
