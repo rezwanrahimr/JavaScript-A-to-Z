ce5a5b9fd7f96589ee9e063d164e9fb13f28e9f1
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-26 19:41:39 +0600

    pratice problem 3

--- api/programmingHeroJs/practiceProblem.js
+++ api/programmingHeroJs/practiceProblem.js
@@ -6,10 +6,15 @@ var result = takeMony - (apples + orange);
 console.log("Result : ", result); */
 
 // pratice problme 2
-var mathematics = 80,
+/* var mathematics = 80,
   biology = 60,
   chemistry = 100,
   physics = 77,
   bangla = 80;
 var calculate = (mathematics + biology + chemistry + physics + bangla) / 5;
-console.log(calculate.toFixed(2));
+console.log(calculate.toFixed(2)); */
+
+// Pratice problme 3
+var first = "Hello This is Rezwan Rahim",
+  second = "I am a become a Programmer in the world!";
+console.log(first + " " + second);
