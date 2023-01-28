d718c0ad4e50fbe40c8c5d72f2fc15568106ef1c
rezwanrahimr <rezwanrahim69@gmail.com>
2023-01-28 13:40:06 +0600

    querySelectorall js dom

--- api/script.js
+++ api/script.js
@@ -1,5 +1,12 @@
-function loadData() {
+/* function loadData() {
   fetch("http://127.0.0.1:5500/api/data.json")
     .then((result) => result.json())
     .then((data) => console.log(data));
+} */
+
+let all = document.querySelectorAll(".pratice");
+for (let pratice of all) {
+  pratice.style.width = "500px";
+  pratice.style.height = "100px";
+  pratice.style.border = "2px solid red";
 }
