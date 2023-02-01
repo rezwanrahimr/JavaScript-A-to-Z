6dc08b229fcbdddb2e99c1822312e5e32fc5949a
rezwanrahimr <rezwanrahim69@gmail.com>
2023-02-01 21:17:09 +0600

    sub total calculate

--- shopping Card/script.js
+++ shopping Card/script.js
@@ -3,13 +3,62 @@ document.getElementById("phone-plus").addEventListener("click", function () {
   let phoneQuantity = document.getElementById("phone-quantity");
   let increageQuantity = parseFloat(phoneQuantity.value) + 1;
   phoneQuantity.value = increageQuantity;
+  let phonePrice = document.getElementById("phone-price");
+  let calculatingPrice = parseInt(phonePrice.innerText) + 1000;
+  phonePrice.innerHTML = calculatingPrice;
+  subTotal();
 });
 
 // Phone Minus Bnt
 document.getElementById("phone-minus").addEventListener("click", function () {
   let phoneQuantity = document.getElementById("phone-quantity");
-  if (phoneQuantity.value > 0) {
+  if (phoneQuantity.value > 1) {
     let increageQuantity = parseFloat(phoneQuantity.value) - 1;
     phoneQuantity.value = increageQuantity;
+    let phonePrice = document.getElementById("phone-price");
+    let calculatingPrice = parseInt(phonePrice.innerText) - 1000;
+    phonePrice.innerHTML = calculatingPrice;
+    subTotal();
   }
 });
+
+// Phone Cover plus
+document
+  .getElementById("phone-cover-Plus")
+  .addEventListener("click", function () {
+    let phoneCoverQuantity = document.getElementById("phone-cover-quantity");
+    let increaseCoverQuantity = parseInt(phoneCoverQuantity.value) + 1;
+    phoneCoverQuantity.value = increaseCoverQuantity;
+
+    let phoneCoverPrice = document.getElementById("phone-cover-price");
+    phoneCoverPrice.innerText = parseInt(phoneCoverPrice.innerText) + 350;
+    subTotal();
+  });
+
+// Phone Cover Minus
+document
+  .getElementById("phone-cover-minus")
+  .addEventListener("click", function () {
+    let phoneCoverQuantity = document.getElementById("phone-cover-quantity");
+    if (phoneCoverQuantity.value > 1) {
+      let increaseCoverQuantity = parseInt(phoneCoverQuantity.value) - 1;
+      phoneCoverQuantity.value = increaseCoverQuantity;
+
+      let phoneCoverPrice = document.getElementById("phone-cover-price");
+      phoneCoverPrice.innerText = parseInt(phoneCoverPrice.innerText) - 350;
+      subTotal();
+    }
+  });
+
+// sub total
+function subTotal() {
+  let subTotal = document.getElementById("sub-total");
+  let phoneCoverPrice = document.getElementById("phone-cover-price");
+  let phonePrice = document.getElementById("phone-price");
+
+  subTotal.innerHTML =
+    parseInt(phoneCoverPrice.innerHTML) + parseInt(phonePrice.innerHTML);
+  // tax
+  let tax = document.getElementById("tax");
+  tax.innerHTML = Math.floor(parseInt(subTotal.innerHTML) * 0.001);
+}
