dcb0a5f48ae2206d1c6d8af40ae9bcdce86f6f20
rezwanrahimr <rezwanrahim69@gmail.com>
2023-02-01 19:30:27 +0600

    phone minus btn working

--- shopping Card/script.js
+++ shopping Card/script.js
@@ -1,5 +1,15 @@
+// Phone Pluse Btn
 document.getElementById("phone-plus").addEventListener("click", function () {
   let phoneQuantity = document.getElementById("phone-quantity");
   let increageQuantity = parseFloat(phoneQuantity.value) + 1;
   phoneQuantity.value = increageQuantity;
+});
+
+// Phone Minus Bnt
+document.getElementById("phone-minus").addEventListener("click", function () {
+  let phoneQuantity = document.getElementById("phone-quantity");
+  if (phoneQuantity.value > 0) {
+    let increageQuantity = parseFloat(phoneQuantity.value) - 1;
+    phoneQuantity.value = increageQuantity;
+  }
 });
