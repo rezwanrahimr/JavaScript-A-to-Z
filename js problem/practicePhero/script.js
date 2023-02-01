e7391f7a6f5ad3c2f1798f3d76a2d95d815b957d
rezwanrahimr <rezwanrahim69@gmail.com>
2023-02-01 12:07:40 +0600

    done

--- js problem/practicePhero/script.js
+++ js problem/practicePhero/script.js
@@ -1,7 +1,24 @@
-// withdraw
+// Deposit
 document.getElementById("deposit-btn").addEventListener("click", function () {
   let depositNumber = document.getElementById("deposit-number");
   let getDeposit = depositNumber.value;
-  let deposit = (document.getElementById("deposit-value").innerHTML =
-    getDeposit);
+  let deposit = document.getElementById("deposit-value");
+  deposit.innerHTML = getDeposit;
+
+  // balance
+  let balance = document.getElementById("balance-value");
+  let withdraw = document.getElementById("withdraw-value");
+  balance.innerHTML =
+    parseFloat(deposit.innerText) - parseFloat(withdraw.innerText);
+});
+
+// withdraw
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  let withdrawNumber = document.getElementById("withdraw-number");
+  let getWithdraw = withdrawNumber.value;
+  let withdraw = document.getElementById("withdraw-value");
+  withdraw.innerHTML = getWithdraw;
+  let balance = document.getElementById("balance-value");
+  balance.innerHTML =
+    parseFloat(balance.innerText) - parseFloat(withdraw.innerText);
 });
