a683284baa000d6f835cb7cd827f41e589e80de6
rezwanrahimr <rezwanrahim69@gmail.com>
2023-02-22 21:13:29 +0600

    problem one done

---
+++ ES6/destusuctring.js
@@ -0,0 +1,8 @@
+// object destuctring
+const myDetails = {
+  name: "Rezwan Rahim",
+  roll: 31,
+};
+
+const { name, roll } = myDetails;
+console.log(name, roll);
---
+++ ES6/practiceProblem.js
@@ -0,0 +1,4 @@
+/*  //1. Write an arrow function that will take 3 parameters, will
+multiply the parameters and will return the result. */
+const arrowFunction = (one, two, three) => one * two * three;
+console.log(arrowFunction(2, 2, 2));
