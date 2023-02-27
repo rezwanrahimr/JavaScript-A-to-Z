e4fbe97240dc708cbdd13587024fdbe4150ed007
rezwanrahimr <rezwanrahim69@gmail.com>
2023-02-27 19:10:38 +0600

    map

--- ES6/destusuctring.js
+++ ES6/destusuctring.js
@@ -13,3 +13,14 @@ const myObj = {
 };
 const { name } = myObj;
 console.log(name) */
+
+//
+/* const me = [{ id: 1 }];
+console.log(me[0].id);
+ */
+/* 
+// array map
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+let update = [];
+myArray.map((x) => update.push(x * 2));
+console.log(update); */
