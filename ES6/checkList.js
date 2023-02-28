be461b6ee46a35954de85f2cd19c13bb21b2e769
rezwanrahimr <rezwanrahim69@gmail.com>
2023-02-28 16:09:20 +0600

    problme 2

---
+++ ES6/checkList.js
@@ -0,0 +1,11 @@
+// 1
+let myAge = 22;
+const myName = "Rezwan Rahim Rupak";
+
+// 2
+const myObj = {
+  myAge,
+  education: "Bsc in Computer Science and Engineering",
+};
+let text = `${myName} i am a student! i stady in ${myObj.education}. my age is ${myAge} `;
+console.log(text);
