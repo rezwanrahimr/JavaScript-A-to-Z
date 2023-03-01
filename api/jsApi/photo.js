08e287f6d38dff064b1f4f2a605d284d0a88cf20
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-01 20:32:12 +0600

    another api preatice

--- api/jsApi/photo.js
+++ api/jsApi/photo.js
@@ -6,8 +6,16 @@ function loadImage() {
 }
 
 function allImage(data) {
+  let imageContainer = document.getElementById("image-container");
   data.map((image) => {
     const { id, title, thumbnailUrl } = image;
-    console.log(id, title, thumbnailUrl);
+    let imageDiv = document.createElement("div");
+    imageDiv.classList.add("imag-div");
+    imageDiv.innerHTML = `
+    <h3>${id}</h3>
+    <h3>${title}</h3>
+    <img src="${thumbnailUrl}"></img>
+    `;
+    imageContainer.appendChild(imageDiv);
   });
 }
