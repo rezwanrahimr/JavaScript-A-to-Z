bdd8ae3e203a6bad54fd1f409f43f1925c3d39be
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-01 18:07:42 +0600

    load  daynamic data

---
+++ api/jsApi/post.js
@@ -0,0 +1,20 @@
+function loadPost() {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => allPost(data));
+}
+
+function allPost(data) {
+  let postList = document.getElementById("post-container");
+  for (const datas of data) {
+    const { id, title, body } = datas;
+    let newElement = document.createElement("div");
+    newElement.innerHTML = `
+    <h2>${id}</h2>
+    <h2>${title}</h2>
+    <h2>${body}</h2>
+    `;
+    postList.appendChild(newElement);
+  }
+}
