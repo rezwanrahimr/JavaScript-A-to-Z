546df49c70c658ccc68222f15d3070e29d6e6847
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-03 16:25:00 +0600

    country api practice

--- api/jsApi/country.js
+++ api/jsApi/country.js
@@ -8,15 +8,29 @@ const country = () => {
 const showData = (data) => {
   let countryContainer = document.getElementById("country-container");
   data.map((country) => {
-    const { name, flags, capital } = country;
-    console.log(capital);
+    const { name, flags, capital, cca2 } = country;
+    // console.log(country);
     const newElement = document.createElement("div");
+    newElement.classList.add("country");
     newElement.innerHTML = `
     <h3>Name: ${name.common}</h3>
     <Img src='${flags.png}'></Img>
     <h3>Capital: ${capital ? capital[0] : "No Capital"}</h3>
+    <button onClick="countryDetails('${cca2}')">Details</button>
     `;
     countryContainer.appendChild(newElement);
   });
+};
+const countryDetails = (countryCode) => {
+  console.log("click");
+  const url = `https://restcountries.com/v3.1/alpha/${countryCode}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showCountryData(data));
+};
+
+const showCountryData = (singleCountry) => {
+  const { name, capital } = singleCountry[0];
+  console.log(name, capital);
 };
 country();
