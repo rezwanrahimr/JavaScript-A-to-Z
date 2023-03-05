2d09750f2497851f83cd99e3d14f02b4c55ee417
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-05 22:44:55 +0600

    showing comments on UI

--- api/jsApi/precticeProblem/precticeOne.js
+++ api/jsApi/precticeProblem/precticeOne.js
@@ -2,6 +2,25 @@
 async function loadComments() {
   const response = await fetch("https://jsonplaceholder.typicode.com/comments");
   const data = await response.json();
-  console.log(data);
+  displayComments(data);
 }
+
+const displayComments = (data) => {
+  const commantSection = document.getElementById("comments");
+  data.slice(0, 20).map((comment) => {
+    const { postId, name, email, body } = comment;
+    const newElement = document.createElement("div");
+    newElement.classList.add("comment");
+    newElement.innerHTML = `
+    <h1>PostID: ${postId}</h1>
+    <h1>Name: ${name}</h1>
+    <h1>Email: ${email}</h1>
+    <p>Body: ${body}</p>
+    <button>See Post</button>
+
+    `;
+    commantSection.appendChild(newElement);
+  });
+  //   console.log(data);
+};
 loadComments();
