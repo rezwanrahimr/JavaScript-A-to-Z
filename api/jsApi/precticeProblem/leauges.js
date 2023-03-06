8ebf7dd7c6e72cec37849fb400f86dade595a2f1
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-06 18:36:14 +0600

    team detials load

--- api/jsApi/precticeProblem/leauges.js
+++ api/jsApi/precticeProblem/leauges.js
@@ -2,10 +2,34 @@ const loadLueagus = () => {
   try {
     fetch("https://www.thesportsdb.com/api/v1/json/3/all_leagues.php")
       .then((response) => response.json())
-      .then((data) => console.log(data.leagues));
+      .then((data) => showData(data.leagues));
   } catch (error) {
     console.log(error);
   }
+};
+
+// team data
+const showTeamData = (id) => {
+  const teamContainer = document.getElementById("teamDetails");
+  const url = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+const showData = (data) => {
+  const lueagesContainer = document.getElementById("allLeaues");
+  data.forEach((element) => {
+    const { idLeague, strLeague, strSport } = element;
+    const lueagesDiv = document.createElement("div");
+    lueagesDiv.classList.add("leaugeDiv");
+    lueagesDiv.innerHTML = `
+    <h1>StrLeague: ${strLeague}</h1>
+    <h1>strSpor: ${strSport}</h1>
+    <a href="./team.html" target="_blank"><button onClick="showTeamData(${idLeague})">More Details</button></a>
+    `;
+    lueagesContainer?.appendChild(lueagesDiv);
+  });
 };
 
 loadLueagus();
