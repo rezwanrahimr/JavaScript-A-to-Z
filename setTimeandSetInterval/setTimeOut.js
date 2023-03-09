44f60ddb30d8a3a7665faab63308d4de4a299703
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-09 19:59:20 +0600

    js setInterval

--- setTimeandSetInterval/setTimeOut.js
+++ setTimeandSetInterval/setTimeOut.js
@@ -1,3 +1,14 @@
-setTimeout((element) => {
+/* setTimeout((element) => {
   console.log("js");
 }, 5000);
+
+ */
+
+//setInterval
+let num = 0;
+const inv = setInterval((element) => {
+  console.log(++num);
+  if (num === 10) {
+    clearInterval(inv);
+  }
+}, 1000);
