3c0cff1e6c39a1097d94c99ca1b500fcb3917e23
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-10 19:02:58 +0600

    js prompt

--- shopping Card/exploreBrowserApiAndMethod/script.js
+++ shopping Card/exploreBrowserApiAndMethod/script.js
@@ -2,3 +2,8 @@ function confirmm() {
   const confa = confirm("are you sure !");
   console.log(confa);
 }
+
+function pro() {
+  const name = prompt("What is your Name?", "karim");
+  console.log(name);
+}
