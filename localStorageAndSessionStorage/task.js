f30aa5487fff7101d78af17865af1afb4b99aa85
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-11 18:27:46 +0600

    task 2

--- localStorageAndSessionStorage/task.js
+++ localStorageAndSessionStorage/task.js
@@ -1,7 +1,15 @@
-//1. Some Middle Console Log Will Be Logged after 3.5 Seconds.
+/* //1. Some Middle Console Log Will Be Logged after 3.5 Seconds.
 console.log("Name: Rezwan Rahim");
 console.log("Father Name: Mozaffar Hossain");
 setTimeout(() => {
   console.log("Mother Name: Rokeya Sultana");
 }, 3500);
-console.log("Loaction: Feni Town");
+console.log("Loaction: Feni Town"); */
+
+//2. Take a Number From Users,Using Prompt and Add 250 to that Number.
+function getNumber() {
+  let number = prompt("Enter a Number: ", 0);
+  let sum = parseInt(number) + 250;
+  alert(`The Number is : ${sum}`);
+}
+getNumber();
