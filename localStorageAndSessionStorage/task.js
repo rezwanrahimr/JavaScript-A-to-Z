ce3f1d6330aa9e8be0ee2a02464028172d88bd3b
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-11 20:17:27 +0600

    task 3 done

--- localStorageAndSessionStorage/task.js
+++ localStorageAndSessionStorage/task.js
@@ -6,10 +6,17 @@ setTimeout(() => {
 }, 3500);
 console.log("Loaction: Feni Town"); */
 
-//2. Take a Number From Users,Using Prompt and Add 250 to that Number.
+/* //2. Take a Number From Users,Using Prompt and Add 250 to that Number.
 function getNumber() {
   let number = prompt("Enter a Number: ", 0);
   let sum = parseInt(number) + 250;
   alert(`The Number is : ${sum}`);
 }
-getNumber();
+getNumber(); */
+
+//3. what is cookies,writing in English at least 5-7 lines
+/* 
+cookies are small pices of data that store the user device or computer.commonly cookies are using  keep tracking
+the user performance,session information,  and other information thats user interaction on the web site.
+
+*/
