0b4309581aadb77fcd8204c8c309c58c626dfb93
rezwanrahimr <rezwanrahim69@gmail.com>
2023-03-11 13:05:29 +0600

    set object value in local storage

---
+++ localStorageAndSessionStorage/localStorage.js
@@ -0,0 +1,13 @@
+/*
+
+what can i do in this projects
+1. get the input to the user
+2. set the value in local storage as a object
+3. show the local storage data in UI
+*/
+
+//
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const getName = document.getElementById("input-name").value;
+  console.log(getName);
+});
